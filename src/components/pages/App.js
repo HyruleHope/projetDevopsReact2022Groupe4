dfbd4b54d5d36c2e40f49fde55e9b9b9c5963868
Amelin-Brusa <35231973+HyruleHope@users.noreply.github.com>
2022-06-24 18:20:39 +0200

    [EDIT] Update the header in main page

--- src/components/pages/App.js
+++ src/components/pages/App.js
@@ -1,10 +1,12 @@
 import "../../styles/App.css";
 import React from "react";
 import Landing from "./Landing";
+import Header from "../components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Landing />
     </div>
   );
