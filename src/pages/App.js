4896cc8b93a7c08f4e6043fd75d9e4d07cc10cae
Amelin Brusa <bamelin14@gmail.com>
2022-06-25 21:34:58 +0200

    [FEAT]: added the saved FAQ page and 404 pages

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,30 +1,84 @@
 import "../styles/App.css";
 import React from "react";
 import Header from "../components/Header";
-
-//Function to import on app the whole assets folder, to avoid importing all pictures at once
-function importAll(r) {
-  let images = {};
-  r.keys().map((item) => {
-    images[item.replace("./", "")] = r(item);
-  });
-  return images;
-}
-const assets = importAll(
-  require.context("../assets", false, /\.(png|jpe?g|svg)$/)
-);
+import ActivityCard from "../components/ActivityCard";
+import harley from "../assets/harley.jpg";
+import motoGP from "../assets/motoGP.jpg";
+import bmx from "../assets/bmx.jpg";
+import musicar from "../assets/panda.jpg";
+import vintageExpo from "../assets/vintage-expo.jpg";
+import formule1 from "../assets/formule-1.jpg";
+import tesla from "../assets/tesla.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <header className="App-header">
-        <img src={assets["logo.svg"]} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <body className="landingPage">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="intro">
+        <div className="anim-container">
+          <span className="shutter top"></span>
+          <span className="shutter bottom"></span>
+        </div>
+        <div className="title">
+          <h1>VIVEZ UNE EXPERIENCE INOUBLIABLE</h1>
+        </div>
+        <div className="text">
+          <h1>Découvrez nos activités :</h1>
+        </div>
+        <div className="activity">
+          <ActivityCard
+            text="Balade en Harley Davidson"
+            price="1500"
+            image={harley}
+            link="Harley"
+          />
+          <ActivityCard
+            text="Tour de piste en Moto GP"
+            price="2000"
+            image={motoGP}
+            link="MotoGP"
+          />
+        </div>
+        <div className="activity">
+          <ActivityCard
+            text="Parcours de Moto-cross"
+            price="70"
+            image={bmx}
+            link="Bmx"
+          />
+          <ActivityCard
+            text="Musique en voiture"
+            price="500"
+            image={musicar}
+            link="Musicar"
+          />
+        </div>
+        <div className="activity">
+          <ActivityCard
+            text="Visite du musée Renault"
+            price="20"
+            image={vintageExpo}
+            link="VintageExpo"
+          />
+          <ActivityCard
+            text="Course privée de Formule 1"
+            price="3500"
+            image={formule1}
+            link="Formula1"
+          />
+        </div>
+        <div className="activity">
+          <ActivityCard
+            text="Essaie de la dernière Tesla"
+            price="(Par personne) 250"
+            image={tesla}
+            link="Tesla"
+          />
+        </div>
+      </div>
+    </body>
   );
 }
 
--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,13 +1,86 @@
 import React from "react";
 import Header from "../components/Header";
+import "../styles/Faq.css"
 
 function Faq() {
   return (
     <div className="Faq">
       <Header/>
-      <p>This is the FAQ page</p>
+  <h2 className="FaqTitle">
+    Faq
+  </h2>
+  <div className="layout">
+    <div className="accordion">
+      <div className="accordion__question">
+        <p>Quel services vous proposez ?</p>
+  
+      </div>
+      <div className="accordion__answer">
+        <p>Nous organisons des évènements autour en automobile et moto</p>
+      </div>
+    </div>
+  
+    <div className="accordion">
+      <div className="accordion__question">
+        <p>Comment je peux régler ?</p>
+      </div>
+  
+      <div className="accordion__answer">
+        <p>
+            Vous pouvez payer en ligne ou sur place le jour J, nous acceptons les espèces, chèque et carte bleue.
+        </p>
+      </div>
+    </div>
+
+    <div className="accordion">
+       <div className="accordion__question">
+         <p>Vous acceptez les débutants dans vos activités ?</p>
+       </div>
+    
+       <div className="accordion__answer">
+         <p>
+           Bien sur, nos évènements sont ouverts aux novices comme aux experts, venez comme vous êtes.
+         </p>
+       </div>
+    </div>
+
+    <div className="accordion">
+        <div className="accordion__question">
+            <p>Comment me faire rembourser ?</p>
+        </div>
+        
+        <div className="accordion__answer">
+            <p>
+            Toutes questions liée au remboursement doivents se faire avec notre SAV, disponible à cette addresse: a_brusa@etu-webschoolfactory.fr
+            </p>
+        </div>
+    </div>
+
+    <div className="accordion">
+        <div className="accordion__question">
+            <p>Où sont vos Locaux ?</p>
+        </div>
+            
+        <div className="accordion__answer">
+            <p>
+            Nous ne possédons pas de bureaux officiels, vous pouvez nous rencontrer lors des activités.
+            </p>
+        </div>
+    </div>
+  </div>
     </div>
   );
 }
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
 
 export default Faq;
